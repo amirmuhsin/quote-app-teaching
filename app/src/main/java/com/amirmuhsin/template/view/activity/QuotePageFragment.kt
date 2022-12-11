707bceecc2439e39161ad239332f9056d43c8c0f
Mirmukhsin Sodikov <mirmuhsin.sodiqov@gmail.com>
2022-12-11 17:45:48 +0500

    QuotePagerFragment validated

--- app/src/main/java/com/amirmuhsin/template/view/activity/QuotePageFragment.kt
+++ app/src/main/java/com/amirmuhsin/template/view/activity/QuotePageFragment.kt
@@ -4,11 +4,22 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.amirmuhsin.template.R
 
 class QuotePageFragment : Fragment() {
 
+    private lateinit var tvQuote: TextView
+    private lateinit var tvAuthor: TextView
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View =
         inflater.inflate(R.layout.fragment_quote_page, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        tvQuote = view.findViewById(R.id.tvQuote)
+        tvAuthor = view.findViewById(R.id.tvAuthor)
+    }
 }
