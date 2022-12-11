e53bf41415f1402699e38dda8d5718fc39e48140
Mirmukhsin Sodikov <mirmuhsin.sodiqov@gmail.com>
2022-12-11 17:13:25 +0500

    10 random fake quotes created

--- app/src/main/java/com/amirmuhsin/template/view/activity/MainActivity.kt
+++ app/src/main/java/com/amirmuhsin/template/view/activity/MainActivity.kt
@@ -4,9 +4,23 @@ import android.os.Bundle
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.widget.ViewPager2
 import com.amirmuhsin.template.R
+import com.amirmuhsin.template.data.model.Quote
 import com.amirmuhsin.template.view.adapter.QuotePagerAdapter
 
 class MainActivity : FragmentActivity() {
+
+    private val quoteList = arrayListOf(
+        Quote(1, "Birinchi Quote", "Mirmuhsin"),
+        Quote(2, "Ikkinchi Quote", "Ibrohim"),
+        Quote(3, "Uchinchi Quote", "Najimxon"),
+        Quote(4, "Tortinchi Quote", "Ibrohim"),
+        Quote(5, "Beshinchi Quote", "Mirmuhsin"),
+        Quote(6, "OLdtinci Quote", "Ibrohim"),
+        Quote(7, "Yettinghic Quote", "Najimxon"),
+        Quote(8, "Sakkingizin Quote", "Ibrohim"),
+        Quote(9, "Toqqincih Quote", "Najimxon"),
+        Quote(10, "Onchicnichi Quote", "Ibrohim"),
+    )
 
     private lateinit var viewPager: ViewPager2
 
