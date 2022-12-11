dfde7cfdb412388de345b030326736ba69b1b005
Mirmukhsin Sodikov <mirmuhsin.sodiqov@gmail.com>
2022-12-11 16:58:13 +0500

    rename

--- app/src/main/java/com/amirmuhsin/template/view/adapter/QuotePagerAdapter.kt
+++ app/src/main/java/com/amirmuhsin/template/view/adapter/QuotePagerAdapter.kt
@@ -3,11 +3,11 @@ package com.amirmuhsin.template.view.adapter
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
-import com.amirmuhsin.template.view.activity.ScreenSlidePageFragment
+import com.amirmuhsin.template.view.activity.QuotePageFragment
 
 class QuotePagerAdapter(fa: FragmentActivity) : FragmentStateAdapter(fa) {
 
     override fun getItemCount(): Int = 3
 
-    override fun createFragment(position: Int): Fragment = ScreenSlidePageFragment()
+    override fun createFragment(position: Int): Fragment = QuotePageFragment()
 }
